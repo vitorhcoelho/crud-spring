481551c5c67bead4d735fe71bd44aebf5c6edbf4
Vitor Hugo Coelho <vitor.hugo.cq@live.com>
2025-02-27 00:24:03 -0300

    feat: update course endpoint

--- src/main/java/com/vitorhcoelho/controller/CourseController.java
+++ src/main/java/com/vitorhcoelho/controller/CourseController.java
@@ -2,7 +2,6 @@ package com.vitorhcoelho.controller;
 
 import java.util.List;
 
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,16 +16,16 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-  private final CourseRepository courseRepository;
-
+  private CourseRepository courseRepository;
+  
   @GetMapping
   public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable Long id) {
-    return courseRepository.findById(id).map(record -> ResponseEntity.ok().body(record)).orElse(ResponseEntity.notFound().build()) ;
+  public ResponseEntity<Course> listById(@PathVariable Long id) {
+    return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound)).orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
@@ -34,5 +33,17 @@ public class CourseController {
   public Course create(@RequestBody Course course) {
     return courseRepository.save(course);
   }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(code = HttpStatus.ACCEPTED)
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    return courseRepository.findById(id).map(recordFound -> {
+      recordFound.setName(course.getName());
+      recordFound.setCategory(course.getCategory());
+      Course updated = courseRepository.save(recordFound);
+
+      return ResponseEntity.ok().body(updated);
+    }).orElse(ResponseEntity.notFound().build());
+  };
 
 }
