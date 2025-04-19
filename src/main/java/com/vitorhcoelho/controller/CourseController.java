eb4ae7610fd9bf40a5c1c50ee84544fe1518f1b8
Vitor Hugo Coelho <vitor.hugo.cq@live.com>
2025-04-19 20:12:14 -0300

    refactor: creating service organization

--- src/main/java/com/vitorhcoelho/controller/CourseController.java
+++ src/main/java/com/vitorhcoelho/controller/CourseController.java
@@ -2,6 +2,7 @@ package com.vitorhcoelho.controller;
 
 import java.util.List;
 
+import com.vitorhcoelho.service.CourseService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -13,50 +14,45 @@ import org.springframework.web.bind.annotation.*;
 import com.vitorhcoelho.model.Course;
 import com.vitorhcoelho.repository.CourseRepository;
 
-import lombok.AllArgsConstructor;
-
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor 
 public class CourseController {
 
-  private CourseRepository courseRepository;
+  private final CourseService courseService;
+
+  public CourseController(CourseService courseService) {
+    this.courseService = courseService;
+  }
     
   @GetMapping
   public @ResponseBody List<Course> list() {
-    return courseRepository.findAll();
+    return courseService.list();
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Course> listById(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound)).orElse(ResponseEntity.notFound().build());
+  public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
+    return courseService.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound)).orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
   @ResponseStatus(code = HttpStatus.CREATED)
   public Course create(@RequestBody @Valid Course course) {
-    return courseRepository.save(course);
+    return courseService.create(course);
   }
 
   @PutMapping("/{id}")
   @ResponseStatus(code = HttpStatus.ACCEPTED)
   public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody Course course) {
-    return courseRepository.findById(id).map(recordFound -> {
-      recordFound.setName(course.getName());
-      recordFound.setCategory(course.getCategory());
-      Course updated = courseRepository.save(recordFound);
-
-      return ResponseEntity.ok().body(updated);
-    }).orElse(ResponseEntity.notFound().build());
+    return courseService.update(id, course).map(recordFound -> ResponseEntity.ok().body(recordFound)).orElse(ResponseEntity.notFound().build());
   };
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-    return courseRepository.findById(id).map(recordFound -> {
-      courseRepository.deleteById(id);
+    if(courseService.delete(id)){
       return ResponseEntity.noContent().<Void>build();
-    }).orElse(ResponseEntity.notFound().build());
+    }
+    return ResponseEntity.notFound().build();
   }
 
 }
---
+++ src/main/java/com/vitorhcoelho/service/CourseService.java
@@ -0,0 +1,56 @@
+package com.vitorhcoelho.service;
+
+import com.vitorhcoelho.model.Course;
+import com.vitorhcoelho.repository.CourseRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+@Validated
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> list() {
+        return this.courseRepository.findAll();
+    }
+
+    public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id);
+    }
+
+    public Course create(@RequestBody @Valid Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+        return courseRepository.findById(id).map(recordFound -> {
+            recordFound.setName(course.getName());
+            recordFound.setCategory(course.getCategory());
+            return courseRepository.save(recordFound);
+        });
+    };
+
+    public boolean delete(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id).map(recordFound -> {
+            courseRepository.deleteById(id);
+            return true;
+        }).orElse(false);
+    }
+
+}
